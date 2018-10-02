6b6abaf23f36e3c5b98c74d27644f812abae48fc
viswasvarmasatya <viswasvarmasatya@msitprogram.net>
2018-10-02 22:35:35 +0530

    "check_style_score -> 6 of 6 passed. style score: 1/1 "

--- m8/8.1 Team Ranking With Insertion/8.1 Team Ranking With Insertion/Solution.java
+++ m8/8.1 Team Ranking With Insertion/8.1 Team Ranking With Insertion/Solution.java
@@ -1,49 +1,49 @@
-import	java.util.Arrays;
-import	java.util.Scanner;
+import  java.util.Arrays;
+import  java.util.Scanner;
 /**
  * Class for solution.
  */
-public class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**.
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		// StringBuffer sb = new StringBuffer();
-		String input = "";
-		// int count = 0;
-		while (scan.hasNextLine()) {
-			// sb.append(scan.nextLine() + "::");
-			input += scan.nextLine() + "::";
-			// count++;
-		}
-		String[] inputLines = input.split("::");
-		Team[] teamArr = new Team[inputLines.length];
-		final int p = 0;
-		final int q = 1;
-		final int r = 2;
-		final int t = 3;
-		for (int i = 0; i < teamArr.length; i++) {
-			String[] teamTockens = inputLines[i].split(",");
-			teamArr[i] = new Team(teamTockens[p],
-			 Integer.parseInt(teamTockens[q]),
-			  Integer.parseInt(teamTockens[r]),
-			   Integer.parseInt(teamTockens[t]));
-		}
-		// String[] str = sb.toString().split("::");
-		Selection s = new Selection();
-		s.sort(teamArr);
-		String output = Arrays.toString(teamArr);
-		String op =
-		 output.replace("[", "").replace("]", "").replace(" ", "");
-		System.out.println(op);
-	}
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        // StringBuffer sb = new StringBuffer();
+        String input = "";
+        // int count = 0;
+        while (scan.hasNextLine()) {
+            // sb.append(scan.nextLine() + "::");
+            input += scan.nextLine() + "::";
+            // count++;
+        }
+        String[] inputLines = input.split("::");
+        Team[] teamArr = new Team[inputLines.length];
+        final int p = 0;
+        final int q = 1;
+        final int r = 2;
+        final int t = 3;
+        for (int i = 0; i < teamArr.length; i++) {
+            String[] teamTockens = inputLines[i].split(",");
+            teamArr[i] = new Team(teamTockens[p],
+             Integer.parseInt(teamTockens[q]),
+              Integer.parseInt(teamTockens[r]),
+               Integer.parseInt(teamTockens[t]));
+        }
+        // String[] str = sb.toString().split("::");
+        Selection s = new Selection();
+        s.sort(teamArr);
+        String output = Arrays.toString(teamArr);
+        String op =
+         output.replace("[", "").replace("]", "").replace(" ", "");
+        System.out.println(op);
+    }
 }
