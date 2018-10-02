4221f3326a9ae880b763f92b5017ecdfe1d1ccc4
viswasvarmasatya <viswasvarmasatya@msitprogram.net>
2018-10-02 22:44:06 +0530

    "check_style_score -> 6 of 6 passed. style score: 1/1 "

--- m8/8.1 Team Ranking With Insertion/8.1 Team Ranking With Insertion/Selection.java
+++ m8/8.1 Team Ranking With Insertion/8.1 Team Ranking With Insertion/Selection.java
@@ -1,12 +1,34 @@
+/**.
+ * Class for selection.
+ */
 class Selection	{
+	/**
+	 * { compares two elements in an array }
+	 *
+	 * @param      element1  The element 1 of comparable type
+	 * @param      element2  The element 2 of comparable type
+	 * complexity 1
+	 * @return     { returns -1 if it staisfies conditon else returns 1}
+	 */
 	public boolean LessThan(Comparable element1, Comparable element2) {
 		return element1.compareTo(element2) < 0;
 	}
+	/**
+	 * { exchanges two elements in comparable array }
+	 * complexity 1
+	 * @param      a       { comparable array}
+	 * @param      index1  The index 1 of array
+	 * @param      index2  The index 2 of array 
+	 */
 	public void exchange(Comparable []a, int index1, int index2) {
 		Comparable temp = a[index1];
 		a[index1] = a[index2];
 		a[index2] = temp;
 	}
+	/**
+	 * { sorts all the elements in array }
+	 * complexity N**2
+	 */
 	public void sort(Comparable a[]) {
 		// Comparable min = a[i];
 		int N = a.length;
@@ -21,15 +43,15 @@ class Selection	{
 			}
 		}
 	}
-	public int min(Comparable []a, int i) {
-		Comparable min = a[i];
-		int minIndex = i;
-		for (i = i+1; i < a.length; i++) {
-			if (LessThan(a[i], min)) {
-				min = a[i];
-				minIndex = i;
-			}
-		}
-		return minIndex;
-	}
+	// public int min(Comparable []a, int i) {
+	// 	Comparable min = a[i];
+	// 	int minIndex = i;
+	// 	for (i = i+1; i < a.length; i++) {
+	// 		if (LessThan(a[i], min)) {
+	// 			min = a[i];
+	// 			minIndex = i;
+	// 		}
+	// 	}
+	// 	return minIndex;
+	// }
 }	
